f66380c48c0c3ffc6d6654a7bcfd51bd6d28d468
Ovi Sofariu <ovi@sofariu.com>
2023-12-27 22:28:33 -0500

    day1 parts 1,2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,59 @@
- class Day01 {
-      fun part1(input: List<String>): Int {
-          return input.sumOf { extractCalibrationCode(it) }
-      }
+class Day01 {
+    fun part1(input: List<String>): Int {
+        return input.sumOf { this.extractSimpleCalibrationCode(it) }
+    }
 
-     private fun extractCalibrationCode(calibrationText: String): Int {
-         val firstDigit = calibrationText.first { it.isDigit() }.toString().toInt()
-         val lastDigit = calibrationText.last { it.isDigit() }.toString().toInt()
-         return 10 * firstDigit + lastDigit;
-     }
+    private fun extractSimpleCalibrationCode(calibrationText: String): Int {
+        val firstDigit = calibrationText.first { it.isDigit() }.toString().toInt()
+        val lastDigit = calibrationText.last { it.isDigit() }.toString().toInt()
+        return 10 * firstDigit + lastDigit;
+    }
+
+    fun part2(testInput: List<String>): Int {
+        return testInput.sumOf { this.extractComplexCalibrationCode(it) }
+    }
+
+    private fun extractComplexCalibrationCode(calibrationText: String): Int {
+        val digitMap = mapOf(
+            "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5,
+            "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
+        )
+        val firstDigit: Int = findDigit(calibrationText, digitMap)
+        val lastDigit = findDigit(calibrationText.reversed(),
+            digitMap.entries.map { it.key.reversed() to it.value }.associateBy({ it.first }, { it.second })
+        )
+        return firstDigit * 10 + lastDigit
+    }
+
+    private fun findDigit(calibrationText: String, digitMap: Map<String, Int>): Int {
+        val firstDigit: Int = calibrationText.foldIndexed(0) { index: Int, acc: Int, c: Char ->
+            if (acc == 0 && c.isDigit()) {
+                c.toString().toInt()
+            } else if (acc == 0 && charMatchesDigits(digitMap, calibrationText, index)) {
+                mapCharToDigit(digitMap, calibrationText, index)
+            } else
+                acc
+        }
+        return firstDigit
+    }
+
+    private fun charMatchesDigits(
+        digitMap: Map<String, Int>,
+        calibrationText: String,
+        index: Int
+    ): Boolean {
+        val matcher: (String) -> Boolean = { calibrationText.substring(index).startsWith(it) }
+        return digitMap.keys.any(matcher)
+    }
+
+    private fun mapCharToDigit(digitMap: Map<String, Int>, calibrationText: String, index: Int): Int {
+        val matcher: (key: String) -> Boolean = { calibrationText.substring(index).startsWith(it) }
+        return digitMap[digitMap.keys.first(matcher)].toString().toInt()
+    }
 }
 
- fun main(args: Array<String>) {
-     val input = readInput("day01")
-     println("input for " + input.size + " records: " + Day01().part1(input))
- }
+fun main(args: Array<String>) {
+    val input = readInput("day01")
+    println("part 1 input for " + input.size + " records: " + Day01().part1(input))
+    println("part 2 input for " + input.size + " records: " + Day01().part2(input))
+}
--- testSrc/Day01KtTest.kt
+++ testSrc/Day01KtTest.kt
@@ -10,7 +10,39 @@ class Day01KtTest {
     }
     @Test
     fun providedExample() {
-        val testInput = readInput("day01_test")
+        val testInput = readInput("day01_1")
         expectThat(Day01().part1(testInput)).isEqualTo(142)
+    }
+
+    @Test
+     fun checkRegexReplacement() {
+         expectThat(Regex("one").replace("3one4one", "1")).isEqualTo("3141")
+     }
+
+    @Test
+    fun simpleExample() {
+        expectThat(Day01().part2(listOf("4nine"))).isEqualTo(49)
+    }
+
+    @Test
+    fun trickyExampleMapSomeDigits() {
+        val testInput = listOf("eightwothree", "14gxqgqsqqbxfpxnbccjc33eight")
+        expectThat(Day01().part2(testInput)).isEqualTo(83+18)
+    }
+
+    @Test
+    fun anotherTrickyExample() {
+        expectThat(Day01().part2(listOf("five5nzlcdc45clclzrrkjthreeoneoneightsd"))).isEqualTo(58)
+    }
+
+    @Test
+    fun trivialExampleMapAllDigits() {
+        val testInput = listOf("one","two", "three", "four", "five", "six", "seven", "eight", "nine")
+        expectThat(Day01().part2(testInput)).isEqualTo(495)
+    }
+    @Test
+    fun providedExample2() {
+        val testInput = readInput("day01_2")
+        expectThat(Day01().part2(testInput)).isEqualTo(281)
     }
 }
