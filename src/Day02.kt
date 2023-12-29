7d54cee001c99e7a7b7aaebc7fd849746483410b
Ovi Sofariu <ovi@sofariu.com>
2023-12-29 12:24:41 -0500

    finished day2 part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,24 +4,23 @@ class Day02(private val games: List<String>) {
     }
 
     fun part1(cubesAvailable: Map<Color, Int>): Int {
-        return games.sumOf {  sumValidGameIDs(it, cubesAvailable) }
+        return games.sumOf { sumValidGameIDs(it, cubesAvailable) }
     }
-    private fun sumValidGameIDs(game: String, cubesAvailable:Map<Color, Int>): Int {
-        val (gameId: String, rounds: String) = extractGameIdAndRounds(game)
-        val roundsForGame = rounds.split(";")
 
-        return if (roundsForGame.all { roundValid(it, cubesAvailable) })
+    private fun sumValidGameIDs(game: String, cubesAvailable: Map<Color, Int>): Int {
+        val (gameId: String, rounds: String) = extractGameIdAndRounds(game)
+        return if (rounds.split(";").all { roundValid(it, cubesAvailable) })
             gameId.toInt()
         else
             0
     }
 
-    private fun roundValid(round: String, cubesAvailable:Map<Color, Int>): Boolean {
-        return round.split(",").all { c -> haveEnoughOfColor(c, cubesAvailable ) }
+    private fun roundValid(round: String, cubesAvailable: Map<Color, Int>): Boolean {
+        return round.split(",").all { c -> haveEnoughOfColorForCube(c, cubesAvailable) }
     }
 
-    private fun haveEnoughOfColor(colorEntry: String, cubesAvailable:Map<Color, Int>): Boolean {
-        val (count, color) = Regex("(\\d+)\\s(\\w+)").matchEntire(colorEntry)!!.destructured
+    private fun haveEnoughOfColorForCube(colorEntry: String, cubesAvailable: Map<Color, Int>): Boolean {
+        val (count, color) = extractColorCounts(colorEntry)
         return cubesAvailable[Color.valueOf(color)]!! >= count.toInt()
     }
 
@@ -31,15 +30,47 @@ class Day02(private val games: List<String>) {
             .replace("; ", ";")
             .replace(", ", ",")
 
-        val (gameId, roundsString) = Regex("Game (\\d+):(.+)$").matchEntire(trimmedGame)!!.destructured
+        val (gameId, roundsString) = extractGameIdAndRoundsForGame(trimmedGame)
         return Pair(gameId, roundsString)
     }
-}
-fun makeCube(red: Int, green: Int, blue: Int): Map<Day02.Color, Int> {
-    return mapOf(Day02.Color.red to red, Day02.Color.green to green, Day02.Color.blue to blue)
+
+    fun part2(): Int {
+        return games.sumOf { sumMinimumPowersForGame(it) }
+    }
+    private fun sumMinimumPowersForGame(game: String): Int {
+        val (_: String, rounds: String) = extractGameIdAndRounds(game)
+        val aggregateCounts = rounds.split(";").fold(ColorCounts()) { acc, round ->
+            applyMinimumCountsForRound(round, acc)
+        }
+        return aggregateCounts.red * aggregateCounts.green * aggregateCounts.blue
+    }
+
+    private fun applyMinimumCountsForRound(round: String, colorCounts: ColorCounts): ColorCounts {
+        return round.split(",").fold(colorCounts) { acc, colorEntry ->
+            val (count, color) = extractColorCounts(colorEntry)
+            acc.applyMaximumCountForColor(color, count.toInt())
+        }
+    }
+
+    private fun extractGameIdAndRoundsForGame(trimmedGame: String) =
+        Regex("Game (\\d+):(.+)$").matchEntire(trimmedGame)!!.destructured
+
+    private fun extractColorCounts(colorEntry: String) =
+        Regex("(\\d+)\\s(\\w+)").matchEntire(colorEntry)!!.destructured
+
+    data class ColorCounts(val red: Int = 0, val green: Int = 0, val blue: Int = 0) {
+        fun applyMaximumCountForColor(color: String, count: Int): ColorCounts {
+            return when (color) {
+                "red" -> copy(red = Math.max(red, count))
+                "blue" -> copy(blue = Math.max(blue, count))
+                "green" -> copy(green = Math.max(green, count))
+                else -> this
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
     val input = readInput("day02")
-    println("part 1 input for " + input.size + " records: " + Day02(input).part1(makeCube(12, 13, 14) ))
+    println("part 2 input for " + input.size + " records: " + Day02(input).part2() )
 }
--- testSrc/Day02Test.kt
+++ testSrc/Day02Test.kt
@@ -3,9 +3,6 @@ import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
 class Day02Test {
-// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-
-
     @Test
     fun part1Simple() {
         val games = listOf("Game 123: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
@@ -21,4 +18,20 @@ class Day02Test {
         expectThat(Day02(games).part1(makeCube(12, 13 ,14))).isEqualTo(1)
         expectThat(Day02(games).part1(makeCube(12, 6 ,14))).isEqualTo(0)
     }
+
+    @Test
+    fun part2Simple() {
+        val games = listOf("Game 1: 3 green, 1 blue, 3 red; 3 blue, 1 green, 3 red; 2 red, 12 green, 7 blue; 1 red, 4 blue, 5 green; 7 green, 2 blue, 2 red")
+        expectThat(Day02(games).part2()).isEqualTo(12*7*3)
+    }
+
+    @Test
+    fun part2providedExample() {
+        val testInput = readInput("day02")
+        expectThat(Day02(testInput).part2()).isEqualTo(2286)
+    }
+}
+
+fun makeCube(red: Int, green: Int, blue: Int): Map<Day02.Color, Int> {
+    return mapOf(Day02.Color.red to red, Day02.Color.green to green, Day02.Color.blue to blue)
 }
