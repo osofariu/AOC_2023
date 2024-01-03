5288d6217b1a921f677c4820bdd535aac9beb5c5
Ovi Sofariu <ovi@sofariu.com>
2024-01-03 10:20:42 -0500

    day4

--- src/Day04.kt
+++ src/Day04.kt
@@ -8,7 +8,7 @@ class Day04(private val scratchCards: List<String>) {
     }
     private fun processCard(card: String): Int {
         val (win: List<Int>, have: List<Int>) = extractNumbers(card)
-        return scoreCard(win, have)
+        return scoreCardPart1(win, have)
     }
 
     private fun extractNumbers(card: String): Pair<List<Int>,List<Int>> {
@@ -18,14 +18,46 @@ class Day04(private val scratchCards: List<String>) {
     }
     private fun intValues(intStrings: String) = intStrings.split(" ").filter { it != "" }.map { it.toInt() }
 
-    private fun scoreCard(winList: List<Int>, haveList: List<Int>): Int {
-       val winningCards: Int = haveList.fold(0) { acc: Int, number: Int ->
-           if (winList.contains(number)) acc + 1 else acc
-       }
-        return 2.0.pow(winningCards - 1.0).toInt()
+    private fun scoreCardPart1(winList: List<Int>, haveList: List<Int>): Int {
+       val winningCards: Int = winningCardsCount(haveList, winList)
+        println("wins: $winningCards, points: ${2.0.pow(winningCards - 1.0).toInt()}")
+        return 2.0.pow( winningCardsCount(haveList, winList) - 1.0).toInt()
+    }
+
+    private fun winningCardsCount(haveList: List<Int>, winList: List<Int>) =
+        haveList.fold(0) { acc: Int, number: Int ->
+            if (winList.contains(number)) acc + 1 else acc
+        }
+
+    fun part2(): Int {
+        val scratchCardsEarned = scratchCards
+            .mapIndexed { index, card -> generateScratchCards(index+1, card) }
+            .toMap().toMutableMap()
+        (scratchCards.size-1).downTo(1).forEach { index ->
+                updateScratchCardsForIndex(index, scratchCardsEarned)
+        }
+        return scratchCardsEarned.values.sumOf { it.size } + scratchCardsEarned.size
+    }
+    private fun generateScratchCards(index: Int, card: String): Pair<Int, List<Int>> {
+        val (win: List<Int>, have: List<Int>) = extractNumbers(card)
+        val points = winningCardsCount(win, have)
+        val children =(index+ 1..index+points).toList()
+        return Pair(index, children)
+    }
+    private fun updateScratchCardsForIndex(cardToUpdate: Int, scratchCards: MutableMap<Int, List<Int>>): Unit {
+        val cardsToMultiply = scratchCards[cardToUpdate]!!.orEmpty()
+        (1..scratchCards.size).forEach {indexToUpdate ->
+            scratchCards[indexToUpdate] =
+                if (scratchCards[indexToUpdate]!!.contains(cardToUpdate)) {
+                    cardsToMultiply.plus(scratchCards[indexToUpdate]!!)
+                } else {
+                    scratchCards[indexToUpdate].orEmpty()
+                }
+        }
     }
 }
 fun main() {
     val input = readInput("day04")
     println("part 1 input for " + input.size + " sum: " + Day04(input).part1() )
+    println("part 2 input for " + input.size + " sum: " + Day04(input).part2() )
 }
--- testSrc/Day04Test.kt
+++ testSrc/Day04Test.kt
@@ -5,8 +5,13 @@ import strikt.assertions.isEqualTo
 class Day04Test {
 
     @Test
-    fun givenExample() {
+    fun givenExamplePart1() {
         val testInput = readInput("day04")
         expectThat(Day04(testInput).part1()).isEqualTo(13)
+    }
+    @Test
+    fun givenExamplePart2() {
+        val testInput = readInput("day04part2")
+        expectThat(Day04(testInput).part2()).isEqualTo(30)
     }
 }
