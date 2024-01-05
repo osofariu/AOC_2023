001097b20d2ea944e062c628923eafa65fb8abe9
Ovi Sofariu <ovi@sofariu.com>
2024-01-05 11:05:21 -0500

    complete day 6 part 2

--- src/day6/Day06.kt
+++ src/day6/Day06.kt
@@ -3,32 +3,44 @@ package day6
 import readInput
 
 class Day06(private val input: List<String>) {
-    fun part1(): Int {
-        val races: List<Pair<Int, Int>> = parseRaces(input)
-        return races
+    fun part1() = parseRaces()
             .map { waysToWin(it) }
             .reduce { acc, it -> acc * it }
-    }
-    private fun parseRaces(input: List<String>): List<Pair<Int, Int>> {
-        fun extractNumbersFromLine(rowText: String, rowIndex: Int) =
-            Regex("$rowText([ \\d]+)$").matchEntire(input[rowIndex])!!.destructured
-            .component1().split(" ").filter { it != "" }.map { it.toInt() }
 
-        val times = extractNumbersFromLine("Time:", 0)
-        val distances = extractNumbersFromLine("Distance:", 1)
+    private fun parseRaces(): List<Pair<Long, Long>> {
+        val times: List<Long> = extractNumericsFromLine("Time:", 0)
+        val distances: List<Long> = extractNumericsFromLine("Distance:", 1)
         return times.zip(distances)
     }
-    private fun waysToWin(race: Pair<Int, Int>): Int {
+    private fun extractNumericsFromLine(rowText: String, rowIndex: Int) =
+        extractNumberStringsFromLine(rowText, rowIndex).map {it.toLong()}
+
+    private fun extractNumberStringsFromLine(rowText: String, rowIndex: Int): List<String> =
+        Regex("$rowText([ \\d]+)$").matchEntire(input[rowIndex])!!.destructured
+            .component1().split(" ").filter {
+                it != ""
+            }
+
+    private fun waysToWin(race: Pair<Long, Long>): Int {
         val (recordTime, distance) = race
-        return (1..<recordTime)
+        return (1.toLong()..<recordTime)
             .map { buttonPress ->
                 buttonPress * (recordTime - buttonPress)}
             .filter { it > distance }
             .size
+    }
+
+    fun part2() = waysToWin(parsePart2Race())
+
+    private fun parsePart2Race(): Pair<Long, Long> {
+        val raceTime = extractNumberStringsFromLine("Time:", 0).reduce {acc,s -> acc + s}.toLong()
+        val  raceDistance = extractNumberStringsFromLine("Distance:", 1).reduce {acc,s -> acc + s}.toLong()
+        return Pair(raceTime, raceDistance)
     }
 }
 
 fun main() {
     val input = readInput("day06")
     println("part 1 input for " + input.size + "  " + Day06(input).part1() )
+    println("part 2 input for " + input.size + "  " + Day06(input).part2() )
 }
--- testSrc/day6/Day06Test.kt
+++ testSrc/day6/Day06Test.kt
@@ -8,8 +8,14 @@ import strikt.assertions.isEqualTo
 class Day06Test {
 
     @Test
-    fun `given Example, find ways to win`() {
+    fun `given Example, find ways to win part 1`() {
        val testInput = readInput("day06")
         expectThat(Day06(testInput).part1()).isEqualTo(288)
+    }
+
+    @Test
+    fun `given Example, find ways to win part 2`() {
+        val testInput = readInput("day06")
+        expectThat(Day06(testInput).part2()).isEqualTo(71503)
     }
 }
