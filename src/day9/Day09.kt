890c1203bc9caeef8fe2e4f7f8e546f648e3afbe
Ovi Sofariu <ovi@sofariu.com>
2024-01-15 11:55:58 -0500

    day9 part 2

--- src/day9/Day09.kt
+++ src/day9/Day09.kt
@@ -2,7 +2,7 @@ package day9
 
 import readInput
 
-class Day09(val input : List<String>) {
+class Day09(val input: List<String>) {
     fun part1(): Long {
         return parseAsInts(input)
             .sumOf { predictNext(it) }
@@ -14,7 +14,7 @@ class Day09(val input : List<String>) {
 
     private fun predictNext(input: List<Int>): Long {
         val differenceLists = generateDifferenceLists(input, emptyList())
-        val extendedDifferenceLists = extendDifferenceLists(differenceLists)
+        val extendedDifferenceLists = extendDifferenceLists(differenceLists, Direction.RIGHT)
         return extendedDifferenceLists[0].last().toLong()
     }
 
@@ -27,19 +27,37 @@ class Day09(val input : List<String>) {
         }
     }
 
-    private fun extendDifferenceLists(hist: List<List<Int>>) : List<List<Int>> {
+    enum class Direction {
+        LEFT, RIGHT
+    }
+
+    private fun extendDifferenceLists(hist: List<List<Int>>, direction: Direction): List<List<Int>> {
         return hist.reversed()
             .fold(emptyList<List<Int>>()) { acc: List<List<Int>>, currentList: List<Int> ->
                 if (currentList.all { it == 0 }) {
                     acc + listOf((currentList + 0))
-                } else {
+                } else if (direction == Direction.RIGHT) {
                     acc + listOf((currentList + (acc.last().last() + currentList.last())))
+                } else {
+                    acc + listOf((listOf((currentList.first() - acc.last().first())) + currentList ))
                 }
             }.reversed()
+    }
+
+    fun part2(): Long {
+        return parseAsInts(input)
+            .sumOf { predictPrevious(it) }
+    }
+
+    private fun predictPrevious(list: List<Int>): Long {
+        val differenceLists = generateDifferenceLists(list, emptyList())
+        val extendedDifferenceLists = extendDifferenceLists(differenceLists, Direction.LEFT)
+        return extendedDifferenceLists[0].first().toLong()
     }
 }
 
 fun main() {
     val input = readInput("day09")
     println("part 1 input for " + input.size + "  " + Day09(input).part1())
+    println("part 2 input for " + input.size + "  " + Day09(input).part2())
 }
--- testSrc/day9/Day09Test.kt
+++ testSrc/day9/Day09Test.kt
@@ -8,8 +8,14 @@ import strikt.assertions.isEqualTo
 class Day09Test {
 
     @Test
-    fun `given provided input the sum of predicted next values us 114`() {
+    fun `given provided input for part1, the sum of predicted next values is 114`() {
         val testInput = readInput("day09")
         expectThat(Day09(testInput).part1()).isEqualTo(114)
+    }
+
+    @Test
+    fun `given provided input for part2, the sum of predicted next values is 2`() {
+        val testInput = readInput("day09")
+        expectThat(Day09(testInput).part2()).isEqualTo(2)
     }
 }
