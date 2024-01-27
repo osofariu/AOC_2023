fa4775c94cef96c56c9199ff78280976735a8f32
Ovi Sofariu <ovi@sofariu.com>
2024-01-27 15:39:24 -0500

    day 11 complete

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -3,50 +3,52 @@ package day11
 import readInput
 import kotlin.math.abs
 
-typealias Galaxy = Pair<Int, Int>
+typealias Galaxy = Pair<Long, Long>
 
 class Day11(val input: List<String>) {
-    fun part1(): Int {
+    fun part1(): Long {
         val expandIndices = expandIndicesHorizontally(input)
         val expandedCosmos: List<String> = input.flatMap {
             val expandedRow = expandRow(it, expandIndices)
             if (it.any { item -> item == '#' }) listOf(expandedRow) else listOf(
                 expandedRow,
-                emptyRow(expandedRow.length)
+                emptyRow(expandedRow.length.toLong())
             )
         }
         return sumMinDistances(expandedCosmos)
     }
 
-    private fun expandIndicesHorizontally(input: List<String>): List<Int> =
-        (0..<input[0].length).fold(emptyList()) { acc: List<Int>, i ->
+    private fun expandIndicesHorizontally(input: List<String>): List<Long> =
+        (0..<input[0].length).fold(emptyList()) { acc: List<Long>, i ->
             val columnIsEmpty = (0..input.indices.last).all { input[it][i] == '.' }
-            if (columnIsEmpty) acc + i else acc
+            if (columnIsEmpty) acc + i.toLong() else acc
         }
 
-    private fun expandRow(row: String, expandIndices: List<Int>): String {
+    private fun expandRow(row: String, expandIndices: List<Long>): String {
         return if (expandIndices.isEmpty()) row
         else if (expandIndices.size == 1)
-            row.substring(0, expandIndices[0]) + "." + row.substring(expandIndices[0])
+            row.substring(0, expandIndices[0].toInt()) + "." + row.substring(expandIndices[0].toInt())
         else {
             (listOf(0) + expandIndices).windowed(2)
                 .joinToString(".") {
-                    row.substring(it[0], it[1])
-                } + "." + row.substring(expandIndices.last())
+                    row.substring(it[0].toInt(), it[1].toInt())
+                } + "." + row.substring(expandIndices.last().toInt())
         }
     }
 
-    private fun sumMinDistances(cosmos: List<String>): Int {
-        val galaxies = cosmos.flatMapIndexed { i, y ->
-            val xIndices = y.withIndex().filter { it.value == '#' }
-            xIndices.map {
-                Pair(i, it.index)
-            }
-        }
+    private fun sumMinDistances(cosmos: List<String>): Long {
+        val galaxies = galaxyLocations(cosmos)
         return listCombinations(galaxies)
             .sumOf { (g1: Galaxy, g2: Galaxy) ->
                 abs(g1.first - g2.first) + abs(g1.second - g2.second)
             }
+    }
+
+    private fun galaxyLocations(cosmos: List<String>) = cosmos.flatMapIndexed { i, y ->
+        val xIndices = y.withIndex().filter { it.value == '#' }
+        xIndices.map {
+            Pair(i.toLong(), it.index.toLong())
+        }
     }
 
     private fun <T> listCombinations(list: List<T>): List<Pair<T, T>> {
@@ -61,11 +63,33 @@ class Day11(val input: List<String>) {
         return indexPairs.map { Pair(list[it.first], list[it.second]) }
     }
 
-    private fun emptyRow(rowLength: Int): String =
-        ".".repeat(rowLength)
+    private fun emptyRow(rowLength: Long): String =
+        ".".repeat(rowLength.toInt())
+
+    fun part2(expansionFactor: Int): Long {
+        val expandHorizontalIndices = expandIndicesHorizontally(input)
+        val expandVerticalIndices = expandIndicesVertically(input)
+        val galaxies = galaxyLocations(input)
+        return listCombinations(galaxies)
+            .sumOf { (g1: Galaxy, g2: Galaxy) ->
+                val (g1y, g2y) = listOf(g1.first, g2.first).sorted()
+                val verticalExpansionSlots = expandVerticalIndices.filter { it in (g1y..g2y) }.size
+                val (g1x, g2x) = listOf(g1.second, g2.second).sorted()
+                val horizontalExpansionSlots = expandHorizontalIndices.filter { it in (g1x..g2x) }.size
+                (g2y - g1y - verticalExpansionSlots) + (verticalExpansionSlots * expansionFactor) + (g2x - g1x - horizontalExpansionSlots) + (horizontalExpansionSlots * expansionFactor)
+            }
+    }
+
+    private fun expandIndicesVertically(input: List<String>): List<Long> =
+        List(input.size) { index ->
+            Pair(index, (input[index].none { it == '#' }))
+        }.filter { it.second }.map { it.first.toLong() }
 }
 
 fun main() {
     val input = readInput("day11")
     println("part 1 input for " + input.size + "  " + Day11(input).part1())
+    val expansionFactor = 1_000_000
+    println("part 2 input for " + input.size + "  " + Day11(input).part2(expansionFactor))
 }
+
--- testSrc/day11/Day11Test.kt
+++ testSrc/day11/Day11Test.kt
@@ -10,7 +10,17 @@ class Day11Test {
     @Test
     fun `first example`() {
         val input = readInput("day11")
-        val sumOfDistance = Day11(input).part1()
         expectThat(Day11(input).part1()).isEqualTo(374)
+    }
+
+    @Test
+    fun `second example for part 2 with expansion factor of 10`() {
+        val input = readInput("day11")
+        expectThat(Day11(input).part2(10)).isEqualTo(1030)
+    }
+    @Test
+    fun `second example for part 2 with expansion factor of 100`() {
+        val input = readInput("day11")
+        expectThat(Day11(input).part2(100)).isEqualTo(8410)
     }
 }
